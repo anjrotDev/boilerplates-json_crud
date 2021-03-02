5736778db3c3c387822f8f85655b7d90e0a3921c
anjrotDev <antonio@anjrot.cl>
2021-03-02 10:18:40 -0300

    Initial commit

---
+++ helpers/crud.js
@@ -0,0 +1,40 @@
+const axios = require("../config/axios");
+
+module.exports = {
+  find: async path => {
+    try {
+      const { data } = await axios.get(path);
+
+      return data;
+    } catch (error) {
+      throw error.message;
+    }
+  },
+  create: async (path, body) => {
+    try {
+      const { data } = await axios.post(path, body);
+
+      return data;
+    } catch (error) {
+      throw error.message;
+    }
+  },
+  update: async (path, body) => {
+    try {
+      const { data } = await axios.put(path, body);
+
+      return data;
+    } catch (error) {
+      throw error.message;
+    }
+  },
+  erase: async path => {
+    try {
+      await axios.delete(path);
+
+      return "El usuario fue borrado correctamente!!!";
+    } catch (error) {
+      throw error.message;
+    }
+  }
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+const app = require("./app");
+
+app.listen(4000, () => console.log("Servidor Levantado en el puerto 4000!!!"));
